9deb036adaad63e5902a7cf79d3e716f46d483a0
hornoo <rchrdhorne1@gmail.com>
2015-10-01 22:01:21 +1300

    Version one - fchat6 looks best for further dev

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+	
+	<head>
+	
+	
+	<!--
+	Name: 
+	-->
+	
+	
+	<title></title>
+	
+	<meta charset="UTF-8">
+	
+	<link rel="stylesheet" type="text/css" href="">
+	
+	
+	
+
+	</head>
+	
+	<body>
+	
+		<?php
+		echo ("hello");
+		#phpinfo();
+		?>
+		
+		<img src="scat.php" alt="scat test"></img>
+		<a href="fchart1.html"> Fchart test1 </a>
+		<a href="fchart2.html"> Fchart test2 </a>
+		<a href="fchart3.html"> Fchart test3 </a>
+		
+	</body>
+	
+</html>
